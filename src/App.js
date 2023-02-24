8031d973ba8aaebfd88ed1860cacb75eafa09921
G4zO <kamilgrzelinski@sastrans.pl>
2023-02-24 11:41:15 +0100

    coś tam

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css'
+import {SelectPicker} from 'rsuite';
+import  ReactDOM  from 'react';
+import {fetchMessages} from '/AutoEmailBackup/API.js';
+class Inbox extends React.Component {
+  state = {
+    accountId: '',
+    messages: [],
+  };
 
-function App() {
+  async componentDidMount() {
+    const messages = await fetchMessages(this.state.accountId);
+    this.setState({ messages });
+  }
+
+  handleAccountChange = async (value) => {
+    const messages = await fetchMessages(value);
+    this.setState({ accountId: value, messages });
+  };
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="inbox">
+      <div className="inbox-header">
+        <h1>Odczytywanie Wiadomości z bazy danch</h1>
+        <SelectPicker
+          data={accountOptions}
+          value={this.state.accountId}
+          onChange={this.handleAccountChange}
+        />
+      </div>
+      <div className="inbox-messages">
+        {this.state.messages.map((message) => (
+          <div className="inbox-message" key={message._id}>
+            <div className="inbox-message-header">
+              <h2>{message.title}</h2>
+              <p>Wiadomość od: {message.from}</p>
+            </div>
+            <div className="inbox-message-body">
+              <p>{message.body}</p>
+              {message.attachment && <p>Załącznik: {message.attachment}</p>}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
+}
 
-export default App;
+export default Inbox;
+
+
